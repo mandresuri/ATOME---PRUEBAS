f9d93e0e2dd699ae32defc2b3c6fa5af424d7581
mandresuri <31737940+mandresuri@users.noreply.github.com>
2018-05-08 11:55:33 -0500

    reporter

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-junit-reporter'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
       require('karma-mocha-reporter'),
