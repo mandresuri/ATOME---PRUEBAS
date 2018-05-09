259a8aeab49df629bc63e395bbf19fc138e2039c
mandresuri <31737940+mandresuri@users.noreply.github.com>
2018-05-08 21:46:57 -0500

    fix karma

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,7 +3,6 @@ import { IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
