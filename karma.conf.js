d43a3e0cb6e0bde1fc90a02246e9144e75c86c85
mandresuri <31737940+mandresuri@users.noreply.github.com>
2018-05-08 21:00:17 -0500

    corregido codigo de karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -3,36 +3,36 @@
  
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular/cli"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-junit-reporter'),
-      require('karma-chrome-launcher'),
-      require('karma-remap-istanbul'),
-      require('karma-mocha-reporter'),
-      require('@angular/cli/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-junit-reporter"),
+      require("karma-chrome-launcher"),
+      require("karma-remap-istanbul"),
+      require("karma-mocha-reporter"),
+      require("@angular/cli/plugins/karma")
     ],
     files: [
-      { pattern: './src/test.ts', watched: false }
+      { pattern: "./src/test.ts", watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      "./src/test.ts": ["@angular/cli"]
     },
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      "text/x-typescript": ["ts","tsx"]
     },
     remapIstanbulReporter: {
       reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
+        html: "coverage",
+        lcovonly: "./coverage/coverage.lcov"
       }
     },
     angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
+      config: "./angular-cli.json",
+      environment: "dev"
     },
-    reporters: ['progress', 'junit'],
+    reporters: ["progress", "junit"],
     junitReporter: {
       outputDir: process.env.NEVERCODE_XUNIT_RESULTS_DIR,
       outputFile: "karma-test-results.xml",
@@ -42,7 +42,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: true
   });
 };
